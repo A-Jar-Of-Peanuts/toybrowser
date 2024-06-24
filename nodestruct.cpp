b765efd3049d249012844bdf3f6aba6303bd61db
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2024-06-24 16:04:17 -0700

    dom

---
+++ nodestruct.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include "nodestruct.h";
+
+using namespace std;
+
+class Document: public NodeType {
+};
+
+class Text: public NodeType {
+    public:
+        string text;
+        Text(string t) {
+            text = t;
+        }
+};
+
+class Element: public NodeType {
+    public:
+        string tagname;
+        unordered_map<string, string> atmap;
+        Element(string tn, unordered_map<string, string> am) {
+            tagname = tn;
+            atmap = am;
+        }
+};
+
+Node* text(vector<Node> children, string t) {
+    Text tx(t);
+    Node* nd = new Node(children, tx);
+    return nd;
+}
+
+Node* element(vector<Node> children, string tn, unordered_map<string, string> am) {
+    Element elem(tn, am);
+    Node* nd = new Node(children, elem);
+    return nd;
+}
+
---
+++ nodestruct.h
@@ -0,0 +1,24 @@
+#ifndef NODESTRUCT_H   
+#define NODESTRUCT_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class NodeType{};
+
+struct Node {
+    std::vector<Node> children;
+    NodeType nt;
+
+    Node(std::vector<Node> c, NodeType n) {
+        children = c;
+        nt = n;
+    }
+};
+
+Node* text(std::vector<Node> children, std::string t);
+Node* element(std::vector<Node> children, std::string tn, std::unordered_map<std::string, std::string> am);
+
+#endif
